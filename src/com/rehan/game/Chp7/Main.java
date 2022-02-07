dee85365f83517f0450ebfbdd9809da4face2d3d
Rehan28 <71427882+Rehan28@users.noreply.github.com>
2022-02-07 20:33:54 +0600

    Initial commit

---
+++ src/com/rehan/game/Chp7/Main.java
@@ -0,0 +1,10 @@
+package com.rehan.game.Chp7;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("My name is Rehan");
+        System.out.println("OOP");
+    }
+}
